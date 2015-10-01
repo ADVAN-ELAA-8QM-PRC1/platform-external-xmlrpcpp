85ff721d2fb0640fbf6f8f4e05c327759d08d3c1
Dan Willemsen <dwillemsen@google.com>
2015-09-30 20:38:31 -0700

    Use all-subdir-cpp-files instead of $(wildcard)
    Bug: 24204119
    Change-Id: I91cd75293a09a673a29b8d52e932ef85874fc239

--- Android.mk
+++ Android.mk
@@ -1,18 +1,13 @@
 LOCAL_PATH := $(call my-dir)
-ifeq ($(HOST_OS), linux)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libxmlrpc++
+LOCAL_MODULE_HOST_OS := linux
 
 LOCAL_CLANG := true
 LOCAL_RTTI_FLAG := -frtti
 LOCAL_CPPFLAGS := -Wall -fexceptions
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src
 
-xmlrpc_src_files := \
-    $(wildcard $(LOCAL_PATH)/src/*.cpp)
-LOCAL_SRC_FILES := \
-    $(xmlrpc_src_files:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES := $(call all-cpp-files-under,src)
 include $(BUILD_SHARED_LIBRARY)
-
-endif # HOST_OS == linux
